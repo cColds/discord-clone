fe8854f8cb6d9a9b91d4a627c698223a4914aa32
cColds <103373668+cColds@users.noreply.github.com>
2023-11-16 00:08:53 -0600

    Hide nav if not logged in

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import Providers from "./providers/Providers";
 import ServerNav from "@/components/nav/ServerNav";
 import SvgMasks from "@/components/svgs/SvgMasks";
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/auth.config";
 
 const ggSans = localFont({
   src: "../public/fonts/gg-sans.woff2",
@@ -16,21 +18,29 @@ export const metadata: Metadata = {
   description: "Built with next",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const data = await getServerSession(authConfig);
+
   return (
     <html lang="en">
       <body className={ggSans.className}>
         <SvgMasks />
-        <Providers>
+        {data ? (
+          <Providers>
+            <div className="flex h-full w-full">
+              <ServerNav />
+              <main className="w-full">{children}</main>
+            </div>
+          </Providers>
+        ) : (
           <div className="flex h-full w-full">
-            <ServerNav />
             <main className="w-full">{children}</main>
           </div>
-        </Providers>
+        )}
       </body>
     </html>
   );
