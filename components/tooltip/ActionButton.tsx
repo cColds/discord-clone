d5d73e655c768f33f9f00c890249b3cff9a6eaa9
cColds <103373668+cColds@users.noreply.github.com>
2023-12-06 01:16:09 -0600

    Fix action button forward ref error

--- components/tooltip/ActionButton.tsx
+++ components/tooltip/ActionButton.tsx
@@ -1,32 +1,31 @@
 import { cn } from "@/lib/utils";
 import ActionTooltip from "./ActionTooltip";
+import { forwardRef } from "react";
 
 type ActionButtonProps = {
   children: React.ReactNode;
   name: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-// TODO: fix forwardRef error (still not sure which component causing it)
+const ActionButton = forwardRef<HTMLButtonElement, ActionButtonProps>(
+  ({ children, className, name, ...props }, ref) => {
+    return (
+      <ActionTooltip content={name}>
+        <button
+          ref={ref}
+          className={cn(
+            "flex justify-center items-center text-interactive-normal hover:text-interactive-hover bg-background-secondary rounded-full w-9 h-9 group-hover:bg-background-tertiary",
+            className
+          )}
+          {...props}
+        >
+          {children}
+        </button>
+      </ActionTooltip>
+    );
+  }
+);
 
-const ActionButton = ({
-  children,
-  className,
-  name,
-  ...props
-}: ActionButtonProps) => {
-  return (
-    <ActionTooltip content={name}>
-      <button
-        className={cn(
-          "flex justify-center items-center text-interactive-normal hover:text-interactive-hover bg-background-secondary rounded-full w-9 h-9 group-hover:bg-background-tertiary",
-          className
-        )}
-        {...props}
-      >
-        {children}
-      </button>
-    </ActionTooltip>
-  );
-};
+ActionButton.displayName = "ActionButton";
 
 export default ActionButton;
