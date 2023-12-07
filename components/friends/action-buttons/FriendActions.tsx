c612ed28e66e0861699290c06e0ff011befe1a52
cColds <103373668+cColds@users.noreply.github.com>
2023-12-07 10:52:56 -0600

    Add unfriend user functionality

--- components/friends/action-buttons/FriendActions.tsx
+++ components/friends/action-buttons/FriendActions.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { Message, More } from "@/components/svgs";
 import ActionButton from "@/components/tooltip/ActionButton";
-import { useRouter } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { unfriendUser } from "@/lib/db/social/friends/unfriendUser";
+import { useSession } from "next-auth/react";
 
 const FriendActions = ({ id }: { id: string }) => {
   const router = useRouter();
+  const { data: session } = useSession();
 
   return (
     <>
@@ -14,9 +25,36 @@ const FriendActions = ({ id }: { id: string }) => {
         <Message />
       </ActionButton>
 
-      <ActionButton name="More" onClick={(e) => e.stopPropagation()}>
-        <More />
-      </ActionButton>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <ActionButton name="More" onClick={(e) => e.stopPropagation()}>
+            <More />
+          </ActionButton>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
+            Start Video Call
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
+            Start Voice Call
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            variant="destructive"
+            onClick={async (e) => {
+              e.stopPropagation();
+
+              console.log("Add remove friend logic");
+
+              await unfriendUser(session?.user.id ?? "", id);
+
+              router.refresh();
+            }}
+          >
+            Remove Friend
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 };
---
+++ lib/db/social/friends/unfriendUser.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import mongoose from "mongoose";
+import User from "@/models/User";
+import dbConnect from "../../dbConnect";
+
+export const unfriendUser = async (userId: string, friendId: string) => {
+  if (!userId) return;
+
+  await dbConnect();
+  const mongooseSession = await mongoose.startSession();
+
+  try {
+    await mongooseSession.withTransaction(async () => {
+      return await Promise.all([
+        User.findByIdAndUpdate(
+          friendId,
+          {
+            $pull: { "social.friends": userId },
+          },
+          { session: mongooseSession }
+        ),
+        User.findByIdAndUpdate(
+          userId,
+          {
+            $pull: {
+              "social.friends": friendId,
+            },
+          },
+          { session: mongooseSession }
+        ),
+      ]);
+    });
+
+    console.log("Unfriended user");
+  } catch (err) {
+    console.error(err);
+  }
+};
