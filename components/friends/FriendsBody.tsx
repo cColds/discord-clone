4c4ec08cf7030245af4215339f1e278a5d9aeec0
cColds <103373668+cColds@users.noreply.github.com>
2023-12-12 13:36:00 -0600

    Add activity bar design

--- components/friends/FriendsBody.tsx
+++ components/friends/FriendsBody.tsx
@@ -24,7 +24,19 @@ export default function FriendsBody({ social, tab }: FriendsBodyProps) {
       )}
 
       <aside className="min-w-[360px] max-w-[420px] basis-[30%] p-4 border-l-[1px] border-background-modifier-accent xl:block hidden">
-        Activity Bar
+        <h2 className="font-extrabold mt-2 mb-4 text-xl text-header-primary">
+          Active Now
+        </h2>
+
+        <div className="flex p-4 justify-center items-center flex-col rounded-lg text-center">
+          <h2 className="mb-1 text-header-primary font-bold">
+            It's quiet for now...
+          </h2>
+          <p className="text-sm text-interactive-normal">
+            When a friend starts an activity—like playing a game or hanging out
+            on voice—we’ll show it here!
+          </p>
+        </div>
       </aside>
     </div>
   );
