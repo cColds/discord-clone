5de9353037a482761921fc4aa4c7dcd4e4879434
cColds <103373668+cColds@users.noreply.github.com>
2024-02-22 19:50:58 -0600

    Add rollover animation, use display name and grow user text container

--- client/components/panels/UserPanel.tsx
+++ client/components/panels/UserPanel.tsx
@@ -23,18 +23,33 @@ export default function UserPanel({
       <div className="mb-[1px] px-2 text-sm h-[52px] flex items-center font-medium">
         <button
           aria-label="Set Status"
-          className="flex min-w-[120px] items-center gap-2 hover:bg-background-modifier-selected border-0 rounded-[4px] grow -ml-0.5 mr-2 pl-0.5"
+          className="group flex min-w-[120px] items-center gap-2 hover:bg-background-modifier-selected border-0 rounded-[4px] grow -ml-0.5 mr-2 pl-0.5"
         >
           <div className="w-8 h-8">
             <AvatarMask username={username} status={status} avatar={avatar} />
           </div>
 
-          <div className="py-1 mr-1 overflow-hidden">
+          <div className="py-1 mr-1 overflow-hidden grow">
             <div>
-              <p className="text-left truncate">{username}</p>
+              <p className="text-left truncate">{displayName}</p>
             </div>
             <div className="text-left text-xs text-header-secondary">
-              <p className="truncate">{status}</p>
+              <div className="relative overflow-hidden">
+                <p className="truncate absolute translate-y-[107%] opacity-0 transition duration-200 ease-in-out group-hover:translate-y-0 group-hover:opacity-100 w-full">
+                  {/* NOTE: Text ellipsis only works when the width has a limit. 
+                      I think the child's content normally respects the parents container as long as the parent has a limit on its width, even if the child has its width set to auto.
+                      But I think in this case, position absolute breaks it because width: auto; is not bounded by the parent anymore.
+                      Fixing this just requires the child's width to be static (not auto), or at most the parent's width.
+                      Tested max width: 100% and width: 100% works but not min-width: 0, probably because of position absolute.
+
+                      Also more solutions: https://stackoverflow.com/questions/31928710/text-overflow-ellipsis-on-dynamic-width-element
+                  */}
+                  {username}
+                </p>
+                <p className="truncate transition duration-200 ease-in-out group-hover:translate-y-[-107%] group-hover:opacity-0">
+                  {status}
+                </p>
+              </div>
             </div>
           </div>
         </button>
