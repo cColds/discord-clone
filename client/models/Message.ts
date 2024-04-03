cd04d8e7eb80489ad5ab5f21deaf92bef78287d2
cColds <103373668+cColds@users.noreply.github.com>
2024-04-03 17:26:57 -0500

    Add channelid to message schema

--- client/models/Message.ts
+++ client/models/Message.ts
@@ -3,12 +3,14 @@ import { Schema, model, models, Types, Document } from "mongoose";
 export interface MessageType extends Document {
   sender: Types.ObjectId;
   message: string;
+  channelId: string;
 }
 
 const MessageSchema = new Schema<MessageType>(
   {
     sender: { type: Schema.Types.ObjectId, required: true },
     message: { type: String, maxLength: 2000, required: true },
+    channelId: { type: String, required: true },
   },
   { timestamps: true }
 );
