cd8c0e0ebd20f868191cbae64ec390cfd51e9d3f
cColds <103373668+cColds@users.noreply.github.com>
2024-05-17 20:00:27 -0500

    Remove unnecessary id field in schema

--- client/models/Server.ts
+++ client/models/Server.ts
@@ -4,33 +4,28 @@ export interface ServerType extends Document {
   serverName: string;
   icon?: string;
   channels: ChannelType[];
-
   categories: CategoryType[];
   members: Types.ObjectId[];
 }
 
 type ChannelType = {
-  id: string;
   type: "text" | "voice";
   name: string;
   messages: Types.ObjectId[];
 };
 
 type CategoryType = {
-  id: string;
   categoryName: string;
   channels: ChannelType[];
 };
 
 const ChannelSchema = new Schema<ChannelType>({
-  id: { type: String, required: true },
   type: { type: String, enum: ["text", "voice"], required: true },
   name: { type: String, required: true },
   messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
 });
 
 const CategorySchema = new Schema({
-  id: { type: String, required: true },
   categoryName: { type: String, required: true },
   channels: [ChannelSchema],
 });
