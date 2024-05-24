61dddc89d37eb9d31a7d8555e5866c71266946f6
cColds <103373668+cColds@users.noreply.github.com>
2024-05-24 15:40:25 -0500

    Remove unnecessary component and fix async error

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -1,5 +1,13 @@
-import Home from "@/components/home/Home";
+import { authConfig } from "@/auth.config";
+import HomeClient from "@/components/home/HomeClient";
+import { getUser } from "@/lib/db/getUser";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return <Home />;
+export default async function HomePage() {
+  const session = await getServerSession(authConfig);
+  const user = await getUser(session?.user.id);
+  if (user == null) redirect("/login");
+
+  return <HomeClient />;
 }
