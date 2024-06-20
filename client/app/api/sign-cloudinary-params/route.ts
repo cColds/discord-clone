685e7a829a8e7f73f3f14263c3a0a9cc57b09f80
Cold <103373668+cColds@users.noreply.github.com>
2024-06-20 01:39:34 -0500

    Fix cloudinary not uploading images

--- client/app/api/sign-cloudinary-params/route.ts
+++ client/app/api/sign-cloudinary-params/route.ts
@@ -1,4 +1,8 @@
-import { v2 as cloudinary } from "cloudinary";
+import {
+  UploadApiErrorResponse,
+  UploadApiResponse,
+  v2 as cloudinary,
+} from "cloudinary";
 import { NextResponse } from "next/server";
 
 cloudinary.config({
@@ -7,13 +11,51 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export async function POST(request: Request) {
-  const timestamp = new Date().getTime();
+type UploadResponse =
+  | { success: true; result?: UploadApiResponse }
+  | { success: false; error: UploadApiErrorResponse };
 
-  const signature = cloudinary.utils.api_sign_request(
-    { timestamp },
-    process.env.CLOUDINARY_API_SECRET as string
-  );
+const uploadToCloudinary = (
+  fileUri: string,
+  fileName: string
+): Promise<UploadResponse> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload(fileUri, {
+        invalidate: true,
+        resource_type: "auto",
+        filename_override: fileName,
+        folder: "discord-clone",
+        use_filename: true,
+      })
+      .then((result) => {
+        resolve({ success: true, result });
+      })
+      .catch((error) => {
+        reject({ success: false, error });
+      });
+  });
+};
 
-  return NextResponse.json({ timestamp, signature });
+export async function POST(req: Request) {
+  const formData = await req.formData();
+  const file = formData.get("file") as File;
+
+  const fileBuffer = await file.arrayBuffer();
+
+  const mimeType = file.type;
+  const encoding = "base64";
+  const base64Data = Buffer.from(fileBuffer).toString("base64");
+
+  // this will be used to upload the file
+  const fileUri = "data:" + mimeType + ";" + encoding + "," + base64Data;
+
+  const res = await uploadToCloudinary(fileUri, file.name);
+
+  if (res.success && res.result) {
+    return NextResponse.json({
+      message: "success",
+      imgUrl: res.result.secure_url,
+    });
+  } else return NextResponse.json({ message: "failure" });
 }
--- client/lib/db/uploadFileToCloudinary.ts
+++ client/lib/db/uploadFileToCloudinary.ts
@@ -3,23 +3,11 @@ export const uploadFileToCloudinary = async (formData: FormData) => {
     `${process.env.NEXT_URL}/api/sign-cloudinary-params`,
     {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-
-  const { signature, timestamp } = await authCloudinary.json();
-
-  const upload = await fetch(
-    `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload?api_key=${process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY}&timestamp=${timestamp}&signature=${signature}`,
-    {
-      method: "POST",
       body: formData,
     }
   );
 
-  const { secure_url } = await upload.json();
+  const authResponse = await authCloudinary.json();
 
-  return secure_url;
+  return authResponse.imgUrl;
 };
