eb9fdc2985797e51c9f36275c6270362aa3c7836
Cold <103373668+cColds@users.noreply.github.com>
2024-07-11 03:00:06 -0500

    Update primary button props

--- client/components/buttons/PrimaryButton.tsx
+++ client/components/buttons/PrimaryButton.tsx
@@ -4,16 +4,18 @@ type PrimaryButtonProps = {
   onClick?: () => void;
   children: React.ReactNode;
   className?: string;
+  type?: "button" | "submit";
 };
 
 const PrimaryButton = ({
   onClick,
   children,
   className,
+  type = "button",
 }: PrimaryButtonProps) => {
   return (
     <button
-      type="button"
+      type={type}
       className={cn(
         "truncate px-4 py-2 h-8 min-w-[60px] text-sm leading-4 min-h-[32px] text-white border-0 bg-brand-500 hover:bg-brand-560 active:bg-brand-600",
         className
