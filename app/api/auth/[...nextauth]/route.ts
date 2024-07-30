e700bc3795029f148d317cb20ce426fe6d193b7f
Cold <103373668+cColds@users.noreply.github.com>
2024-07-30 04:52:23 -0500

    Move auth options to a separate file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,56 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import User from "@/models/User";
-import { authConfig } from "@/auth.config";
-import dbConnect from "@/lib/db/dbConnect";
-import { loginSchema } from "@/lib/validations/login";
-
-const authOptions: AuthOptions = {
-  ...authConfig,
-
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize(credentials) {
-        const parsedCredentials = loginSchema.safeParse(credentials);
-        if (!parsedCredentials.success) return null;
-
-        const { email, password } = parsedCredentials.data;
-
-        await dbConnect();
-
-        const user = await User.findOne({ email }).select({
-          email: 1,
-          username: 1,
-          avatar: 1,
-          status: 1,
-          displayName: 1,
-          password: 1,
-        });
-
-        if (!user) throw new Error("Login or password is invalid.");
-
-        const passwordsMatch = await bcrypt.compare(password, user.password);
-        if (!passwordsMatch) throw new Error("Login or password is invalid.");
-
-        return {
-          id: user.id,
-          email: user.email,
-          username: user.username,
-          displayName: user.displayName,
-          status: user.status,
-          avatar: user.avatar,
-        };
-      },
-    }),
-  ],
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
---
+++ lib/auth.ts
@@ -0,0 +1,53 @@
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import User from "@/models/User";
+import { authConfig } from "@/auth.config";
+import dbConnect from "@/lib/db/dbConnect";
+import { loginSchema } from "@/lib/validations/login";
+
+export const authOptions: AuthOptions = {
+  ...authConfig,
+
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const parsedCredentials = loginSchema.safeParse(credentials);
+        if (!parsedCredentials.success) return null;
+
+        const { email, password } = parsedCredentials.data;
+
+        await dbConnect();
+
+        const user = await User.findOne({ email }).select({
+          email: 1,
+          username: 1,
+          avatar: 1,
+          status: 1,
+          displayName: 1,
+          password: 1,
+        });
+
+        if (!user) throw new Error("Login or password is invalid.");
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (!passwordsMatch) throw new Error("Login or password is invalid.");
+
+        return {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          displayName: user.displayName,
+          status: user.status,
+          avatar: user.avatar,
+        };
+      },
+    }),
+  ],
+};
