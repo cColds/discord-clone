3e15e679a87dfb437ed39c41ad8bc81075b0332f
Cold <103373668+cColds@users.noreply.github.com>
2024-07-30 04:27:29 -0500

    Make settings tab item accessible

--- components/sidebars/UserSettingsSidebar.tsx
+++ components/sidebars/UserSettingsSidebar.tsx
@@ -9,18 +9,20 @@ type TabItemProps = {
 
 const TabItem = ({ name, selected, onTabClick }: TabItemProps) => {
   return (
-    <li
-      className={cn(
-        "py-1.5 px-2.5 mb-0.5 rounded text-md leading-5 truncate list-none text-interactive-normal cursor-pointer hover:text-interactive-hover hover:bg-background-modifier-hover",
-        selected
-          ? "bg-background-modifier-selected cursor-default text-interactive-active"
-          : ""
-      )}
-      aria-selected={selected}
-      aria-label={name}
-      onClick={() => onTabClick(name)}
-    >
-      {name}
+    <li className="list-none w-full">
+      <button
+        aria-label={name}
+        onClick={() => onTabClick(name)}
+        aria-selected={selected}
+        className={cn(
+          " text-left w-full py-1.5 px-2.5 mb-0.5 rounded text-md leading-5 truncate list-none text-interactive-normal cursor-pointer hover:text-interactive-hover hover:bg-background-modifier-hover",
+          selected
+            ? "bg-background-modifier-selected cursor-default text-interactive-active"
+            : ""
+        )}
+      >
+        {name}
+      </button>
     </li>
   );
 };
