24760e3f96f98cfe89898418887bf3005ed4fabc
Cold <103373668+cColds@users.noreply.github.com>
2024-09-09 20:24:42 -0500

    Fix mask id colliding and add another viewbox prop

--- components/svgs/ProfileBanner.tsx
+++ components/svgs/ProfileBanner.tsx
@@ -1,6 +1,8 @@
+import { v4 as uuidv4 } from "uuid";
+
 type ProfileBannerProps = {
   className?: string;
-  viewBox: "0 0 300 105" | "0 0 660 100";
+  viewBox: "0 0 300 105" | "0 0 660 100" | "0 0 340 120";
   previewProfile?: boolean;
 };
 
@@ -9,15 +11,20 @@ export const ProfileBanner = ({
   viewBox,
   previewProfile = false,
 }: ProfileBannerProps) => {
+  const uuid = uuidv4();
+
   return (
     <svg className={className} viewBox={viewBox}>
-      <mask id="profile-pic-top">
+      <mask id={`profile-pic-top-${uuid}`}>
         <rect fill="white" x="0" y="0" width="100%" height="100%"></rect>
         {previewProfile ? (
           <circle fill="black" cx="56" cy="101" r="46"></circle>
         ) : (
           <circle fill="black" cx="62" cy="122" r="46"></circle>
         )}
+        {/* should use if its dm sidebar profile <circle fill="black" cx="56" cy="112" r="46"></circle> */}
+
+        {/* todo: banner size preset like small, med, xl */}
       </mask>
       <foreignObject
         x="0"
@@ -25,8 +32,9 @@ export const ProfileBanner = ({
         width="100%"
         height="100%"
         overflow="visible"
-        mask="url(#profile-pic-top)"
+        mask={`url(#profile-pic-top-${uuid})`}
       >
+        {/* todo: add color thief  or node vibrant to get img color*/}
         <div
           className={`bg-[#2B5269] ${
             previewProfile ? "h-[105px]" : "h-[105px]"
