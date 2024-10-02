49524eacd0a2b0ce1d3b3243d005ad5ad502fd4d
Cold <103373668+cColds@users.noreply.github.com>
2024-10-02 07:30:50 -0500

    Delete unused file

--- lib/services/queries.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getMessages } from "../db/getMessages";
-
-export const useGetMessages = (channelId: string) => {
-  return useQuery({
-    queryKey: ["messages", channelId],
-    queryFn: () => getMessages(channelId),
-  });
-};
